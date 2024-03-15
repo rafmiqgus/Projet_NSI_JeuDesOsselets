8d13f69a19a4aa9563634238a677cbe5a81417f3
xawoken <fh5andmc@gmail.com>
2024-03-15 08:50:30 +0100

    AFFICHAGE DES

--- codeaffichage.py
+++ codeaffichage.py
@@ -7,21 +7,21 @@ def affichage(p,de1,de2,j1,j2):
     tempb = (8 - len(p[1]) )* " "
     print("__________________________________________________________________________________________")
     print("|                                                                                        |")
-    print("|  ",p[0],tempa,"                         |",j1[0][2],"| | | | |               ",de1,"                     |")
-    print("|   score :                             ‾   ‾   ‾                                        |")
-    print("|                                      |",j1[0][1],"| | | | |                                       |")
-    print("|                                       ‾   ‾   ‾                                        |")
-    print("|                                      |",j1[0][0],"| | | | |                                       |")
-    print("|                                       ‾   ‾   ‾                                        |")
+    print("|  ",p[0],tempa,"                         |",j1[0][2],"||",j1[1][2],"||",j1[2][2],"|               ",de1,"                 |")
+    print("|   score :                              ‾    ‾    ‾                                     |")
+    print("|                                      |",j1[0][1],"||",j1[1][1],"||",j1[2][1],"|                                   |")
+    print("|                                        ‾    ‾    ‾                                     |")
+    print("|                                      |",j1[0][0],"||",j1[1][0],"||",j1[2][0],"|                                   |")
+    print("|                                        ‾    ‾    ‾                                     |")
     print("|                                                                                        |")
     print("|                                                                                        |")
     print("|                                                                                        |")
-    print("|  ",p[1],tempb,"                         | | | | | |               ",de2,"                     |")
-    print("|   score :                             ‾   ‾   ‾                                        |")
-    print("|                                      | | | | | |                                       |")
-    print("|                                       ‾   ‾   ‾                                        |")
-    print("|                                      | | | | | |                                       |")
-    print("|                                       ‾   ‾   ‾                                        |")
+    print("|  ",p[1],tempb,"                         |",j2[0][2],"||",j2[1][2],"||",j2[2][2],"|               ",de2,"                 |")
+    print("|   score :                              ‾    ‾    ‾                                     |")
+    print("|                                      |",j2[0][1],"||",j2[1][1],"||",j2[2][1],"|                                   |")
+    print("|                                        ‾    ‾    ‾                                     |")
+    print("|                                      |",j2[0][0],"||",j2[1][0],"||",j2[2][0],"|                                   |")
+    print("|                                        ‾    ‾    ‾                                     |")
     print("|                                                                                        |")
     print("|________________________________________________________________________________________|")
 def choixnom():
@@ -46,7 +46,7 @@ def duel():
     print("Au tour de" ,p[0], " .")
     roll = de()
     print("Vous avez eu un " , roll, " .")
-    placement = str(input("Ou voulez vous placer le dé , Colonne 1,2, ou 3?"))
+    placement = int(input("Ou voulez vous placer le dé , Colonne 1,2, ou 3?"))
     if placement == 1:
         if j1[0][0] == 0:
             j1[0][0]  = roll
@@ -54,7 +54,21 @@ def duel():
             j1[0][1]  = roll
         elif j1[0][2] == 0 and j1[0][1] != 0 and j1[0][0] != 0:
             j1[0][1]  = roll
-    affichage(p,roll,0)
+    elif placement == 2:
+        if j1[1][0] == 0:
+            j1[1][0]  = roll
+        elif j1[1][1] == 0 and j1[1][0] != 0:
+            j1[1][1]  = roll
+        elif j1[1][2] == 0 and j1[1][1] != 0 and j1[1][0] != 0:
+            j1[1][1]  = roll
+    elif placement == 3:
+        if j1[2][0] == 0:
+            j1[2][0]  = roll
+        elif j1[2][1] == 0 and j1[2][0] != 0:
+            j1[2][1]  = roll
+        elif j1[2][2] == 0 and j1[2][1] != 0 and j1[2][0] != 0:
+            j1[2][1]  = roll
+    affichage(p,roll,0,j1,j2)
 
 
 
