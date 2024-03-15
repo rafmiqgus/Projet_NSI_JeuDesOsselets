a80866d79bf3edfffae70c2715fafbee599c6e5e
xawoken <fh5andmc@gmail.com>
2024-03-15 08:37:39 +0100

    ok

--- codeaffichage.py
+++ codeaffichage.py
@@ -2,21 +2,21 @@ import random
 def de():
     return random.randint(1,6)
 
-def affichage(p):
+def affichage(p,de1,de2,j1,j2):
     tempa = (8 - len(p[0]) )* " "
     tempb = (8 - len(p[1]) )* " "
     print("__________________________________________________________________________________________")
     print("|                                                                                        |")
-    print("|  ",p[0],tempa,"                         | | | | | |                                       |")
+    print("|  ",p[0],tempa,"                         |",j1[0][2],"| | | | |               ",de1,"                     |")
     print("|   score :                             ‾   ‾   ‾                                        |")
-    print("|                                      | | | | | |                                       |")
+    print("|                                      |",j1[0][1],"| | | | |                                       |")
     print("|                                       ‾   ‾   ‾                                        |")
-    print("|                                      | | | | | |                                       |")
+    print("|                                      |",j1[0][0],"| | | | |                                       |")
     print("|                                       ‾   ‾   ‾                                        |")
     print("|                                                                                        |")
     print("|                                                                                        |")
     print("|                                                                                        |")
-    print("|  ",p[1],tempb,"                         | | | | | |                                       |")
+    print("|  ",p[1],tempb,"                         | | | | | |               ",de2,"                     |")
     print("|   score :                             ‾   ‾   ‾                                        |")
     print("|                                      | | | | | |                                       |")
     print("|                                       ‾   ‾   ‾                                        |")
@@ -39,10 +39,30 @@ def choixnom():
 
 
 def duel():
+    j1 = [[0,0,0],[0,0,0],[0,0,0]]
+    j2 = [[0,0,0],[0,0,0],[0,0,0]]
     print("Bienvenue ! Vous avez choisi le mode duel (1 contre 1).")
     p = choixnom()
-    
-    
+    print("Au tour de" ,p[0], " .")
+    roll = de()
+    print("Vous avez eu un " , roll, " .")
+    placement = str(input("Ou voulez vous placer le dé , Colonne 1,2, ou 3?"))
+    if placement == 1:
+        if j1[0][0] == 0:
+            j1[0][0]  = roll
+        elif j1[0][1] == 0 and j1[0][0] != 0:
+            j1[0][1]  = roll
+        elif j1[0][2] == 0 and j1[0][1] != 0 and j1[0][0] != 0:
+            j1[0][1]  = roll
+    affichage(p,roll,0)
+
+
+
+
+
 duel()
+    
+
+
 
     
