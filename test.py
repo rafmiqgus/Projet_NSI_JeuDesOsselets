cc9a7f799fd5e40b03974947f32676dcb50ad0b0
rafmiqgus <rafaelmiqgus@gmail.om>
2024-03-19 08:52:37 +0100

    commit

--- test.py
+++ test.py
@@ -17,20 +17,20 @@ vel = 5
 screen.fill("white")
 
 grid = pygame.image.load("images/grid.png")
-screen.blit(grid, (400, 100))
-screen.blit(grid, (400, 450))
+screen.blit(grid, (400, 60))
+screen.blit(grid, (400, 490))
 
 blaireau = pygame.image.load("images/blaireau.png")
-screen.blit(pygame.transform.scale_by(blaireau, 0.3), (125, 500))
+screen.blit(pygame.transform.scale_by(blaireau, 0.3), (125, 540))
 
 hibou = pygame.image.load("images/hibou.png")
-screen.blit(pygame.transform.scale_by(hibou, 0.3), (910, 130))
+screen.blit(pygame.transform.scale_by(hibou, 0.3), (910, 90))
 
-font = pygame.font.SysFont("Arial", 30)
-nom_blaireau = font.render("Blaireau", True, "black")
-screen.blit(nom_blaireau, (125, 550))
-nom_hibou = font.render("Hibou", True, "black")
-screen.blit(nom_hibou, (910, 180))
+font = pygame.font.SysFont("rubikbold", 30)
+nom_blaireau = font.render("Blaireau", True, (80, 227, 194))
+screen.blit(nom_blaireau, (138, 690))
+nom_hibou = font.render("Hibou", True, (184, 233, 134))
+screen.blit(nom_hibou, (935, 280))
 
 # Game loop
 
@@ -50,5 +50,4 @@ pygame.quit()
 fonts = pygame.font.get_fonts()
 print(len(fonts))
 for font in fonts:
-    if font == "PatuaOne" or font == "Patuaone" or font == "patuaone":
-        print("True")
+    print(font)
