c77668aac037b3bd53fae10cd05cd7d7c47f6fbe
rafmiqgus <rafaelmiqgus@gmail.om>
2024-04-01 20:55:19 +0200

    ajout musique skyrim

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 import pygame 
 from affichage import *
 
+init_main_menu()
 init_display(("joueur_1", "joueur_2"))
 
 running = True
 clock = pygame.time.Clock()
-
-add_score_colonne(30, "joueur_1", 3)
-remove_score_colonne("joueur_1", 3)
-
 
 while running:
     for event in pygame.event.get():
